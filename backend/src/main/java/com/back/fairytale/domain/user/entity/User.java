26fe196f7597810765b6ee35ac16a357e6ef111f
yongho9064 <dydgmldydgh@naver.com>
2025-07-23 19:33:17 +0900

    feat: User 엔티티에 keyword, fairytales, favorites 관계 추가

--- backend/src/main/java/com/back/fairytale/domain/user/entity/User.java
+++ backend/src/main/java/com/back/fairytale/domain/user/entity/User.java
@@ -1,10 +1,14 @@
 package com.back.fairytale.domain.user.entity;
 
-
+import com.back.fairytale.domain.fairytale.entity.Fairytale;
+import com.back.fairytale.domain.fairytale.entity.Keyword;
 import com.back.fairytale.domain.user.enums.IsDeleted;
 import com.back.fairytale.domain.user.enums.Role;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -34,4 +38,12 @@ public class User {
     @Column(nullable = false)
     private Role role;
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Keyword> keywords = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Fairytale> fairytales = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Favorites> favorites = new ArrayList<>();
 }
