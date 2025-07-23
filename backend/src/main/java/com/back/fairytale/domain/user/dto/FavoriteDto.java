37cc1b85cfe3d91615da0708f84aac57082e29e9
이용호 <dydgmldydgh@naver.com>
2025-07-23 16:44:59 +0900

    feat: 즐겨찾기 기능 구현 (#13)
    * 현재 멤버와 동화가 나오지 않은 관계로 더미 데이터 사용

---
+++ backend/src/main/java/com/back/fairytale/domain/user/dto/FavoriteDto.java
@@ -0,0 +1,10 @@
+package com.back.fairytale.domain.user.dto;
+
+import lombok.Data;
+
+@Data
+public class FavoriteDto {
+    private Long id;
+    // private Member member;
+    // private Story story;
+}
