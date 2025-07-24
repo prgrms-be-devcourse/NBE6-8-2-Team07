f6f9f18d4cc6ebaa8c038660034d4393800d3d33
shihan00321 <shihan001@naver.com>
2025-07-24 18:14:18 +0900

    Refactor : 인증 객체에서 userId를 받아올 수 있도록 getter 추가

--- backend/src/main/java/com/back/fairytale/global/security/CustomOAuth2User.java
+++ backend/src/main/java/com/back/fairytale/global/security/CustomOAuth2User.java
@@ -1,11 +1,13 @@
 package com.back.fairytale.global.security;
 
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 
 import java.util.Collection;
 import java.util.Map;
 
+@Getter
 public class CustomOAuth2User extends DefaultOAuth2User {
     private final Long id;
 
