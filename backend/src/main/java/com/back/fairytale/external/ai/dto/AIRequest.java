726e2b29353e17f74a1486f6c14c1a344dbac6b5
Jeongmin Lim <simount3@naver.com>
2025-07-24 11:10:53 +0900

    요청, 응답 dto

--- backend/src/main/java/com/back/fairytale/external/ai/dto/AIRequest.java
+++
@@ -1,4 +0,0 @@
-package com.back.fairytale.external.ai.dto;
-
-public class AIRequest {
-}
---
+++ backend/src/main/java/com/back/fairytale/external/ai/dto/GeminiResponse.java
@@ -0,0 +1,11 @@
+package com.back.fairytale.external.ai.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+// Google Gemini API로부터 받은 응답 JSON을 Java 객체로 파싱하기 위한 구조
+@Getter
+@NoArgsConstructor
+public class AIResponse {
+
+}
