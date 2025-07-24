080a2a0b986695996a3654b8f7441fc36765ad93
YouSeok518 <luke2062@naver.com>
2025-07-24 09:35:21 +0900

    키워드 도메인 클래스 추가

---
+++ backend/src/main/java/com/back/fairytale/domain/keyword/dto/KeywordRequestDto.java
@@ -0,0 +1,5 @@
+package com.back.fairytale.domain.keyword.dto;
+
+public record KeywordRequestDto(
+) {
+}
---
+++ backend/src/main/java/com/back/fairytale/domain/keyword/dto/KeywordResponseDto.java
@@ -0,0 +1,4 @@
+package com.back.fairytale.domain.keyword.dto;
+
+public record KeywordResponseDto() {
+}
