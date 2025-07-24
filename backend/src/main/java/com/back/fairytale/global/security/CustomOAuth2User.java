70fa4cba3b0bf51aa98a28a6a013ced4c341e2e4
shihan00321 <shihan001@naver.com>
2025-07-24 12:43:59 +0900

    Feat: OAuth2 로그인 시 필요한 인증 객체 구현

---
+++ backend/src/main/java/com/back/fairytale/global/security/CustomOAuth2User.java
@@ -0,0 +1,16 @@
+package com.back.fairytale.global.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class CustomOAuth2User extends DefaultOAuth2User {
+    private final Long id;
+
+    public CustomOAuth2User(Long id, Collection<? extends GrantedAuthority> authorities, Map<String, Object> attributes, String nameAttributeKey) {
+        super(authorities, attributes, nameAttributeKey);
+        this.id = id;
+    }
+}
