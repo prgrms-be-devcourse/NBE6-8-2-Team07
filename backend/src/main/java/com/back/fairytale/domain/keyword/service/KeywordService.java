e1b9346af627325ae5d9655fa2083fdf57b4b7f3
YouSeok518 <luke2062@naver.com>
2025-07-25 12:54:16 +0900

    키워드 다건조회 구현완료/테스트 성공

--- backend/src/main/java/com/back/fairytale/domain/keyword/service/KeywordService.java
+++ backend/src/main/java/com/back/fairytale/domain/keyword/service/KeywordService.java
@@ -1,7 +1,6 @@
 package com.back.fairytale.domain.keyword.service;
 
 import com.back.fairytale.domain.keyword.dto.KeywordResponseDto;
-import com.back.fairytale.domain.keyword.entity.Keyword;
 import com.back.fairytale.domain.keyword.enums.KeywordType;
 import com.back.fairytale.domain.keyword.repository.KeywordRepository;
 import lombok.RequiredArgsConstructor;
@@ -24,30 +23,11 @@ public class KeywordService {
                 .map(KeywordResponseDto::fromEntity)
                 .collect(Collectors.toList());
     }
-
     // 타입별 키워드 조회 (Enum 타입으로 받음)
     @Transactional(readOnly = true)
     public List<KeywordResponseDto> getKeywordsByType(KeywordType keywordType) {
         return keywordRepository.findByKeywordType(keywordType).stream()
                 .map(KeywordResponseDto::fromEntity)
                 .collect(Collectors.toList());
-    }
-
-    // 키워드 수정
-    @Transactional
-    public void updateKeyword(Long keywordId, String newKeyword, KeywordType newKeywordType) {
-        Keyword keyword = keywordRepository.findById(keywordId)
-                .orElseThrow(() -> new IllegalArgumentException("키워드가 존재하지 않습니다."));
-        keyword.setKeyword(newKeyword);
-        keyword.setKeywordType(newKeywordType);
-    }
-
-    // 키워드 삭제
-    @Transactional
-    public void deleteKeyword(Long keywordId) {
-        if (!keywordRepository.existsById(keywordId)) {
-            throw new IllegalArgumentException("키워드가 존재하지 않습니다.");
-        }
-        keywordRepository.deleteById(keywordId);
     }
 }
