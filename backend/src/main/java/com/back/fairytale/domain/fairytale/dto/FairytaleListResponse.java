c84af25e3f933e19e38a7fa3dd7113267e662873
Jeongmin Lim <simount3@naver.com>
2025-07-26 01:00:06 +0900

    동화전체조회테스트,키워드저장테스트

--- backend/src/main/java/com/back/fairytale/domain/fairytale/dto/FairytaleListResponse.java
+++ backend/src/main/java/com/back/fairytale/domain/fairytale/dto/FairytaleListResponse.java
@@ -2,18 +2,18 @@ package com.back.fairytale.domain.fairytale.dto;
 
 import com.back.fairytale.domain.fairytale.entity.Fairytale;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 public record FairytaleListResponse(
-    Long id,
-    String title,
-    LocalDateTime createdAt
+        Long id,
+        String title,
+        LocalDate createdAt
 ) {
     public static FairytaleListResponse from(Fairytale fairytale) {
         return new FairytaleListResponse(
                 fairytale.getId(),
                 fairytale.getTitle(),
-                fairytale.getCreatedAt()
+                fairytale.getCreatedAt().toLocalDate()
         );
     }
 }
