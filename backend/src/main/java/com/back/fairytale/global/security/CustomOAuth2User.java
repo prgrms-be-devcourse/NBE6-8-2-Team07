2aafbd00dcfbd257fd9329743099be9fa25c832d
shihan00321 <shihan001@naver.com>
2025-07-25 11:12:39 +0900

    Refactor : 인증 객체 타입 변경

--- backend/src/main/java/com/back/fairytale/global/security/CustomOAuth2User.java
+++ backend/src/main/java/com/back/fairytale/global/security/CustomOAuth2User.java
@@ -1,19 +1,39 @@
 package com.back.fairytale.global.security;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 @Getter
-public class CustomOAuth2User extends DefaultOAuth2User {
-    private final Long id;
+@RequiredArgsConstructor
+public class CustomOAuth2User implements OAuth2User {
 
-    public CustomOAuth2User(Long id, Collection<? extends GrantedAuthority> authorities, Map<String, Object> attributes, String nameAttributeKey) {
-        super(authorities, attributes, nameAttributeKey);
-        this.id = id;
+    private final Long id;
+    private final String username;
+    private final String role;
+
+    @Override
+    public String getName() {
+        return username;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return Map.of();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> collection = new ArrayList<>();
+        collection.add((GrantedAuthority) () -> role);
+        return collection;
     }
 
     public Long getId() {
