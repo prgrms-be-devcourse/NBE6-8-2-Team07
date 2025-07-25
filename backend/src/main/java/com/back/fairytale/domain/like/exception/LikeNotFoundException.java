75190a7e2cd94b9e1bbf121f5fc5af8b63f5b71e
yongho9064 <dydgmldydgh@naver.com>
2025-07-25 12:19:41 +0900

    chore: 사소한 주석문 수정

--- backend/src/main/java/com/back/fairytale/domain/like/exception/LikeNotFoundException.java
+++ backend/src/main/java/com/back/fairytale/domain/like/exception/LikeNotFoundException.java
@@ -1,6 +1,6 @@
 package com.back.fairytale.domain.like.exception;
 
-// 즐겨 찾기가 존재하지 않을 때 발생하는 예외 클래스
+// 좋아요가 존재하지 않을 때 발생하는 예외 클래스
 public class LikeNotFoundException extends RuntimeException {
     public LikeNotFoundException(String message) {
         super(message);
