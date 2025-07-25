e289345761c96eb5c38f2c3c582dd531b06d23cd
이용호 <dydgmldydgh@naver.com>
2025-07-25 12:21:24 +0900

    [feat] 좋아요 기능 구현 (#50)
    * feat: 좋아요 기능 구현
    
    * refactor: BookMark 리팩토링
    
    * config: db_dev 관련 ignore
    
    * config: db_dev 관련 ignore
    
    * chore: 사소한 주석문 수정

---
+++ backend/src/main/java/com/back/fairytale/domain/like/dto/LikeDto.java
@@ -0,0 +1,21 @@
+package com.back.fairytale.domain.like.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LikeDto {
+
+    @NotNull
+    private Long userId;
+
+    @NotNull
+    private Long fairytaleId;
+
+}
