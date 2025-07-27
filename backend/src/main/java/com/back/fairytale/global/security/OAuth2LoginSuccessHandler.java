09f2d529d1f3174c4f4e16c6c2d341e87d9793f6
shihan00321 <shihan001@naver.com>
2025-07-27 19:26:27 +0900

    Feat: JWT 발급 및 쿠키 생성 책임을 JWTProvider로 분리

--- backend/src/main/java/com/back/fairytale/global/security/OAuth2LoginSuccessHandler.java
+++ backend/src/main/java/com/back/fairytale/global/security/OAuth2LoginSuccessHandler.java
@@ -15,37 +15,26 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.Iterator;
 
-@Slf4j
-@Component
 @RequiredArgsConstructor
+@Component
+@Slf4j
 public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
 
-    private final JWTUtil jwtUtil;
+    private final JWTProvider jwtProvider;
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         log.info("OAuth2 로그인 성공: {}", authentication.getName());
-        CustomOAuth2User customOAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+        CustomOAuth2User customUser = (CustomOAuth2User) authentication.getPrincipal();
 
-        //fixme jwt 생성할 때 pk를 사용해도 되는지 확인 필요
-        Long userId = customOAuth2User.getId();
-
+        Long userId = customUser.getId();
         String role = authentication.getAuthorities().iterator().next().getAuthority();
-        log.info("OAuth2 Role {}", role);
 
-        String accessToken = jwtUtil.createJwt(userId, role, 10 * 60 * 1000L, "access"); // 유효기간 10분
-        String refreshToken = jwtUtil.createJwt(userId, role, 24 * 60 * 60 * 1000L, "refresh"); // 유효기간 24시간
-        response.addCookie(createCookie(accessToken, "Authorization", 10 * 60));
-        response.addCookie(createCookie(refreshToken, "refresh", 24 * 60 * 60));
+        Cookie accessCookie = jwtProvider.createAccessTokenCookie(userId, role);
+        Cookie refreshCookie = jwtProvider.createRefreshTokenCookie(userId, role);
+
+        response.addCookie(accessCookie);
+        response.addCookie(refreshCookie);
         response.sendRedirect("http://localhost:3000/");
-    }
-
-    private Cookie createCookie(String token, String key, int expiry) {
-        Cookie cookie = new Cookie(key, token);
-        cookie.setMaxAge(expiry);
-//        cookie.setSecure(true); // HTTPS 환경에서만 쿠키 전송
-        cookie.setPath("/");
-        cookie.setHttpOnly(true);
-        return cookie;
     }
 }
