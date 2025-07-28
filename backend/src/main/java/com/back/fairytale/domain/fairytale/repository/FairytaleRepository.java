1668e1a5b65cb1168d59b8872d975061d4934628
Jeongmin Lim <simount3@naver.com>
2025-07-28 09:31:51 +0900

    work

--- backend/src/main/java/com/back/fairytale/domain/fairytale/repository/FairytaleRepository.java
+++ backend/src/main/java/com/back/fairytale/domain/fairytale/repository/FairytaleRepository.java
@@ -4,7 +4,11 @@ import com.back.fairytale.domain.fairytale.entity.Fairytale;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FairytaleRepository extends JpaRepository<Fairytale, Long> {
     List<Fairytale> findAllByOrderByCreatedAtDesc();
+
+    List<Fairytale> findAllByUserIdOrderByCreatedAtDesc(Long userId);
+    Optional<Fairytale> findByIdAndUserId(Long id, Long userId);
 }
