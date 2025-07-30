7d94961b61e0902083c6a8babd1de8df2b3e8c99
Jeongmin Lim <simount3@naver.com>
2025-07-31 01:45:10 +0900

    tt

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    domains: [
-      'images.unsplash.com',
-      'cdn.pixabay.com',
-      'images.pexels.com',
-      'picsum.photos',
-      'via.placeholder.com',
-      'i.postimg.cc' 
-    ]
-  }
 };
 
 export default nextConfig;
