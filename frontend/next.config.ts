d9bd891143f17534945029fc9a71a48cb0f3dabf
Jeongmin Lim <simount3@naver.com>
2025-07-30 17:16:33 +0900

    메인페이지 이미지 추가

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'cdn.pixabay.com',
+      'images.pexels.com',
+      'picsum.photos',
+      'via.placeholder.com',
+      'i.postimg.cc' 
+    ]
+  }
 };
 
 export default nextConfig;
